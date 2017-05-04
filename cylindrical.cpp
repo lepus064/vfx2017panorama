1be887ef26f90946e58b37767a4efaa3aa98f621
lepus064 <b01501122@ntu.edu.tw>
2017-05-05 00:19:04 +0800

    change

--- cylindrical.cpp
+++ cylindrical.cpp
@@ -7,6 +7,5 @@ void cylindrical(Mat& src, vector<cv::KeyPoint> kps, double f){
     Mat result_mat(src.rows,src.cols,CV_8UC3);
     Point2f center((src.rows-1)/2.0,(src.cols-1)/2.0);
     imshow("ee",result_mat);
-    
     waitKey(0);
 }
