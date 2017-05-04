9870f13dbedd02d6829718cadf53f4224f3536f1
lepus064 <b01501122@ntu.edu.tw>
2017-05-05 00:21:56 +0800

    add cylindrical

--- cylindrical.cpp
+++ cylindrical.cpp
@@ -6,6 +6,6 @@ using namespace cv;
 void cylindrical(Mat& src, vector<cv::KeyPoint> kps, double f){
     Mat result_mat(src.rows,src.cols,CV_8UC3);
     Point2f center((src.rows-1)/2.0,(src.cols-1)/2.0);
-    imshow("ee",result_mat);
+    imshow("ee1",result_mat);
     waitKey(0);
 }
