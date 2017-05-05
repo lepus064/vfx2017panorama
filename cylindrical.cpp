637f91cd70b73fd4f443bf41eeab4fe08a2c229c
lepus064 <b01501122@ntu.edu.tw>
2017-05-05 11:04:08 +0800

    cylindrical

--- cylindrical.cpp
+++ cylindrical.cpp
@@ -5,7 +5,18 @@ using namespace cv;
 
 void cylindrical(Mat& src, vector<cv::KeyPoint> kps, double f){
     Mat result_mat(src.rows,src.cols,CV_8UC3);
-    Point2f center((src.rows-1)/2.0,(src.cols-1)/2.0);
+    Point2f center((src.cols-1)/2.0,(src.rows-1)/2.0);
+    for(int i = 0;i<src.cols;i++){
+        for(int j = 0 ;j<src.rows;j++){
+            double x = f*atan((i-center.x)/f);
+            double y = f*(j-center.y)/sqrt((i-center.x)*(i-center.x)+f*f);
+            x = center.x + x;
+            y = center.y + y;
+            Point2f pt(x,y);
+            result_mat.at<Vec3b>(pt) = src.at<Vec3b>(j,i);
+            // cin >> x;
+        }
+    }
     imshow("ee1",result_mat);
     waitKey(0);
 }
